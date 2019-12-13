af1b002db9ceae81282aa71fa928b5be9409ba90
kevinsellei <kevinsellei@gmail.com>
2019-12-13 19:25:46 +0100

    12.13 firebase data add onclick

--- firebase.js
+++ firebase.js
@@ -9,10 +9,47 @@ var firebaseConfig = {
     appId: "1:56442286432:web:82117be36c7932819a6e05",
     measurementId: "G-VYTBF43DY0"
   };
-  // Initialize Firebase
+  // Initialize FirebaseT
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
+  // id
+  function guid() {
+    function _p8(s) {
+        var p = (Math.random().toString(16)+"000000000").substr(2,8);
+        return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+    }
+    return _p8() + _p8(true) + _p8(true) + _p8();
+  }
+
   // Code
 var database = firebase.database();
-database.ref('fazekas-ar-project');   
+// database.ref('fazekas-ar-project');   
+
+function writeUserData(long, lat) {
+  firebase.database().ref(guid()).set({
+    timestamp: Date.now(),
+    longitude: long,
+    latitude: lat
+  });
+}
+
+function getLocation() {
+  alert('alert');
+  if (navigator.geolocation) {
+    console.log('getloc start');
+    navigator.geolocation.getCurrentPosition(setPosition, locerror, {maximumAge:60000, timeout:5000, enableHighAccuracy:true});
+  } else { 
+    alert("Geolocation can not be accessed");
+  }
+}
+
+function locerror(error) {
+  console.log(error);
+}
+
+function setPosition(position) {
+  console.log('setpos run');
+  writeUserData(position.coords.latitude, position.coords.longitude);
+}
+
